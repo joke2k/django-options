f31de68c880c5f4829cb2b23f8b3fe9bdd690b28
Meng Zhuo <mengzhuo1203@gmail.com>
2013-03-15 16:17:23 +0800

    i18n models

--- django_options/models.py
+++ django_options/models.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-s
 from django.db import models
+from django.utils.translation import ugettext as _
 from django.contrib.sites.models import Site
 from picklefield import PickledObjectField
 from .managers import OptionManager
@@ -8,13 +10,19 @@ class Option(models.Model):
 
     site = models.ForeignKey(Site, related_name='options')
 
-    key= models.CharField(max_length=255)
-    value= PickledObjectField(null=True, default=True)
-    autoload=models.BooleanField(default=False)
+    key = models.CharField(max_length=255,
+                           verbose_name=_('Key'))
+    value = PickledObjectField(null=True, default=True,
+                               verbose_name=_('Value'))
+    autoload = models.BooleanField(default=False,
+                                   verbose_name=_('Autoload'))
 
-    updated_at=models.DateTimeField(auto_now=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    expires_at=models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name=_('Update at'))
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name=_('Create at'))
+    expires_at = models.DateTimeField(blank=True, null=True,
+                                      verbose_name=_('Expires at'))
 
     # override default manager
     objects = OptionManager()
@@ -23,6 +31,5 @@ class Option(models.Model):
 
     class Meta:
         unique_together = ('site', 'key',)
-
-
-
+        verbose_name = _('Option')
+        verbose_name_plur = _('Options')
